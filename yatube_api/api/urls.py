d880e9423a679c01e09b9917db357b0a54869e64
Евгений <cemuk@bk.ru>
2022-02-06 16:56:34 +0300

    added prefix api in main url

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -11,6 +11,6 @@ v1_router.register(r'posts/(?P<post_id>\d+)/comments',
                    CommentViewSet, basename='comments')
 
 urlpatterns = [
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
-    path('api/v1/', include(v1_router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(v1_router.urls)),
 ]
